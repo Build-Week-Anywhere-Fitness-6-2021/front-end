816a4eee18048cfe19bd77a434f5d41d2df4f344
Brett Smith <brettsmith212@gmail.com>
2021-12-20 14:59:36 -0800

    Sign Up Styling for Instructor and Member

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,70 +1,61 @@
-import React from 'react'
-import UserHeader from './UserHeader';
-import { useState } from 'react';
+import React from "react";
+import UserHeader from "./UserHeader";
+import { useState } from "react";
+import "../css/SignUp.css";
 
-const initialValues = [{
+const initialValues = [
+  {
     firstName: "First name*",
     lastName: "Last name*",
     email: "Email address*",
-    password: "Password*"
-}];
+    password: "Password*",
+  },
+];
 
 const SignUp = () => {
-    const [values, setValues] = useState(initialValues);
+  const [values, setValues] = useState(initialValues);
 
+  return (
+    <div className="signUp-container">
+      <UserHeader />
+      <form className="formSignUp-container">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          name="firstName"
+          value={values.firstName}
+          onChange={(e) => setValues(e.target.value)}
+          autocomplete="on"
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={values.lastName}
+          onChange={(e) => setValues(e.target.value)}
+          autocomplete="on"
+          placeholder="Last Name"
+        />
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={(e) => setValues(e.target.value)}
+          autocomplete="on"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={(e) => setValues(e.target.value)}
+          autocomplete="on"
+          placeholder="Password"
+        />
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <UserHeader />
-            <form className='login'>
-                <h2>Create your account</h2>
-                <label>
-                    First Name
-                    <input 
-                        type="text" 
-                        name="firstName" 
-                        value={values.firstName}
-                        onChange={(e) => setValues(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <label>
-                    Last Name
-                    <input 
-                        type="text" 
-                        name="lastName" 
-                        value={values.lastName}
-                        onChange={(e) => setValues(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <label>
-                    Email Address
-                    <input 
-                        type="email" 
-                        name="email" 
-                        value={values.email}
-                        onChange={(e) => setValues(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <label>
-                    Password
-                    <input 
-                        type="password" 
-                        name="password" 
-                        value={values.password}
-                        onChange={(e) => setValues(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <button type="submit">
-                    Create Account
-                </button>
-            </form>
-        </div>
-    )
-}
-
-export default SignUp
+export default SignUp;
