4eedf12bd5e2a9419864c839e64177a6261c228b
david-aihe <doa9200@gmail.com>
2021-12-20 15:39:26 -0500

    Working on Search CSS

--- src/components/SearchClasses.js
+++ src/components/SearchClasses.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import UserHeader from './UserHeader'
+import "../css/SearchClasses.css";
 
 const SearchClasses = () => {
     return (
-        <div>
+        <div className='search-header-container'>
             <UserHeader />
-            <h1>Awaken to a healthier, happier you.</h1>
-            <h3>Discover a world of in-person and virtual fitness, wellness, and beauty services.</h3>
-            <form>
-                <label htmlFor="search">Search for classes</label>
-                <input type="text"/>
-                <button type="submit">Search</button>
-            </form>
+            <div className='search-container'>
+                <h1>Awaken to a healthier, happier you.</h1>
+                <h3>Discover a world of in-person and virtual fitness, wellness, and beauty services.</h3>
+                <form>
+                    <label htmlFor="search"></label>
+                    <div className='searchBox'>
+                        <input 
+                        placeholder='Search for classes'
+                        type="text"/>
+                        <button className="searchBtn" type="submit">Search</button>
+                    </div>
+                    
+                </form> 
+            </div>
         </div>
     )
 }
