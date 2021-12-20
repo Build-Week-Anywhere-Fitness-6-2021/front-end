0cacc5d607d47334be28d556dbd66bd57d92b545
Brett Smith <brettsmith212@gmail.com>
2021-12-20 11:30:13 -0800

    login connected to css | styling added

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,49 +1,44 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import UserHeader from './UserHeader';
+import UserHeader from "./UserHeader";
+import "../css/Login.css";
 
 const UserLogin = () => {
-    const [userEmail, setUserEmail] = useState("");
-    const [userPassword, setUserPassword] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    
-    return (
-        <div>
-            <UserHeader />
-            <form onSubmit={handleSubmit} className="login">
-                <h1>Anywhere Fitness Members</h1>
-                <label>
-                    Email Address
-                    <input 
-                        type="email" 
-                        name="email" 
-                        value={userEmail}
-                        onChange={(e) => setUserEmail(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <label>
-                    Password
-                    <input 
-                        type="password" 
-                        name="password" 
-                        value={userPassword}
-                        onChange={(e) => setUserPassword(e.target.value)}
-                        autocomplete="on"
-                    />
-                </label>
-                <button type="submit">
-                    Login
-                </button>
-                <p>Don't have an account?</p><Link to="/usersignup">Sign up</Link>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="login-container">
+      <UserHeader />
+      <form onSubmit={handleSubmit} className="form-container">
+        <h1>Anywhere Fitness Members</h1>
+        <input
+          type="email"
+          name="email"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+          autoComplete="on"
+          placeholder="Email Address"
+        />
+        <input
+          type="password"
+          name="password"
+          value={userPassword}
+          onChange={(e) => setUserPassword(e.target.value)}
+          autoComplete="on"
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+        <p>Don't have an account?</p>
+        <Link to="/usersignup">Sign up</Link>
+      </form>
+    </div>
+  );
+};
 
-export default UserLogin
+export default UserLogin;
