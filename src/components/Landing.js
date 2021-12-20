60d19d25085b977a7605f1fabd66164939380721
ted <tvolchko@gmail.com>
2021-12-20 12:01:34 -0600

    landing page BGI hover effects added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,13 +7,21 @@ const Landing = () => {
 
     return (
         <div className="container">
+            
             <div className="user">
+            <div className='userBGI'></div>
+                <nav className='landingNav'>
                 <Link to="/searchclasses"><button>SEARCH CLASSES</button></Link>
                 <Link to="/userlogin">USER LOGIN</Link>
+                </nav>
+            
             </div>
             <div className="instructor">
+                <div className='instructorBGI'></div>
+                <nav className='landingNav'>
                 <button>EXPLORE THE SOFTWARE</button>
                 <Link to="/instructorlogin">INSTRUCTOR LOGIN</Link>
+                </nav>
             </div>
         </div>
     )
