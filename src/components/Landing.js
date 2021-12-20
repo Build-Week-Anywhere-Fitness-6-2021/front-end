6232dabbdc78ca0a849b57fcdcd3aa5c111fa6a8
ted <tvolchko@gmail.com>
2021-12-20 13:21:30 -0600

    landing page splash divs added, hover transition

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,18 +10,26 @@ const Landing = () => {
             
             <div className="user">
             <div className='userBGI'></div>
-                <nav className='landingNav'>
-                <Link to="/searchclasses"><button>SEARCH CLASSES</button></Link>
-                <Link to="/userlogin">USER LOGIN</Link>
+                <nav className='userLandingNav'>
+                <Link to="/userlogin"><p>USER LOGIN</p></Link>
                 </nav>
-            
+            <div className='userInfo'>
+                <h2>Find a class perfect for you!</h2>
+                <p> At Anywhere Fitness we have the perfect program <br/>for all of your fitness needs! </p>
+                <Link to="/searchclasses"><button>SEARCH CLASSES</button></Link>
             </div>
+            </div>
+
             <div className="instructor">
                 <div className='instructorBGI'></div>
-                <nav className='landingNav'>
-                <button>EXPLORE THE SOFTWARE</button>
-                <Link to="/instructorlogin">INSTRUCTOR LOGIN</Link>
+                <nav className='instructorLandingNav'>
+                    <Link to="/instructorlogin">INSTRUCTOR LOGIN</Link>
                 </nav>
+                <div className='instructorInfo'>
+                    <h2>Explore our tools for <br/>creating the perfect course!</h2>
+                    <p>We have the resources to help you create and host<br/> the ideal programs for you and your students!</p>
+                    <button>EXPLORE THE SOFTWARE</button>
+                </div>
             </div>
         </div>
     )
