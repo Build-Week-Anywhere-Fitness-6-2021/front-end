986c483d4e82f582724a31085325a8ca030e15c5
ted <tvolchko@gmail.com>
2021-12-21 09:05:13 -0600

    landing css polished

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import '../css/Landing.css';
 import { Link } from "react-router-dom";
-import { styled } from 'styled-components'
-
-const linkStyle = {
-    textDecoration: 'none',
-    color: 'grey',
-    fontWeight: 'bold',
-    transition: '0.1s',
-    '&:hover': {
-        color: '#F58A07',
-        borderBottom: '4px solid #F58A07',
-    }
-}
 
 
 const Landing = () => {
@@ -23,7 +11,7 @@ const Landing = () => {
             <div className="user">
             <div className='userBGI'></div>
                 <nav className='userLandingNav'>
-                <Link to="/userlogin" style={linkStyle}>USER LOGIN</Link>
+                <Link to="/userlogin">USER LOGIN</Link>
                 </nav>
             <div className='userInfo'>
                 <h2>Find a class perfect for you!</h2>
@@ -35,7 +23,7 @@ const Landing = () => {
             <div className="instructor">
                 <div className='instructorBGI'></div>
                 <nav className='instructorLandingNav'>
-                    <Link to="/instructorlogin" style={linkStyle}>INSTRUCTOR LOGIN</Link>
+                    <Link to="/instructorlogin">INSTRUCTOR LOGIN</Link>
                 </nav>
                 <div className='instructorInfo'>
                     <h2>Explore our tools for <br/>creating the perfect course!</h2>
