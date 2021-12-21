7201c28e1f1b280a9104d31b97e77426224223f6
ted <tvolchko@gmail.com>
2021-12-21 12:47:55 -0600

    User searching for classes functionality

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ const classes = [
     {
         name: "Fart Yoga",
         type: "yoga",
-        start: "7:00a.m.",
+        start: "7:00 a.m.",
         duration: 30,
         intensity: "low",
         location: "north",
@@ -12,13 +12,43 @@ const classes = [
     {
         name: "Cycle Sadness",
         type: "cycle",
-        start: "9:00a.m.",
+        start: "9:00 a.m.",
         duration: 60,
         intensity: "high",
         location: "south",
         registered: 3,
         maxsize: 8
-    }
+    },
+    {
+        name: "Boulder Crushing",
+        type: "insanity",
+        start: "11:00 a.m.",
+        duration: 60,
+        intensity: "high",
+        location: "south",
+        registered: 5,
+        maxsize: 10
+    },
+    {
+        name: "Pierre's Pilates",
+        type: "pilates",
+        start: "8:00 a.m.",
+        duration: 30,
+        intensity: "low",
+        location: "west",
+        registered: 1,
+        maxsize: 6
+    },
+    {
+        name: "GET RIPPED XTREME",
+        type: "ripped",
+        start: "3:00 p.m.",
+        duration: 60,
+        intensity: "high",
+        location: "east",
+        registered: 3,
+        maxsize: 8
+    },
 ]
 
 export default classes
